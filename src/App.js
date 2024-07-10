2eaced5b92f7abec7c49119b1e378623493de9d6
sk3316 <sk3316@srmist.edu.in>
2024-07-11 02:12:34 +0530

    commit;

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+// import HomeCards from './components/JobListings';
+import JobListings from './components/JobListings';
 
 function App() {
+  const name = 'Shitanshu';
+  const x = 10;
+  const y = 20;
+  const names = ['Randiket', 'Vibs', 'Aryan', 'sita mayia', 'ram'];
+  const loggedIn = false;
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <h1 className="text-6xl">
+        Hello {name}!
+      </h1>
+      <hr></hr>
+      <p className='text-5xl'> The sum of {x} and {y} is {x + y}. </p>
+      <hr></hr>
+      <ul className='text-4xl'>
+        {names.map((name, index) => (<li key={index}>{name}</li>))}
+      </ul>
+      {loggedIn ? <h1> hello member</h1> : <h1>hello guest</h1>} */}
+
+        <Navbar/>
+        <Hero/>
+        <JobListings/>
+    </>
   );
 }
 
